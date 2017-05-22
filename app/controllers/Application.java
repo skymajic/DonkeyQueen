b11aee4756e52815f15a0dd4b6dc0ce40360a794
tanakamuneo <hukuchan100@gmail.com>
2017-05-22 17:49:27 +0900

    auth layout was done.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,7 +13,17 @@ public class Application extends Controller {
     
     // ページ遷移サンプル
     public static Result rarara() {
-    	return ok(rarara.render("This is rarara."));
+    	return ok(rarara.render("This is a rarara."));
     }
+    
+    public static Result register() {
+    	return ok(register.render("新規会員登録情報を入力してください。"));
+    }
+    
+    public static Result login() {
+    	return ok(login.render("ログイン情報を入力してください。"));
+    }
+    
+    
 
 }
